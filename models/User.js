6482274f52fa7b506fbe080df1bc8bc6cea9f20c
David Rauf <davidrauf@gmail.com>
2024-07-08 18:10:50 -0400

    starting layout

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address.']
+    },
+    
+})
+
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    
+    try {
+
+    }
+})
+
+module.exports = router
